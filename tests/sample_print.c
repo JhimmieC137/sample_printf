a111a1c1ac1b06c564b25404e29de38bf676888e
JhimmieC137 <toluwalope.david@gmail.com>
2023-07-24 03:38:27 +0100

    Started project with base template

---
+++ tests/sample_print.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _printf - Works just like the printf function
+ * 
+ * @format: pointer to characters
+ * Return: 0 on Success
+ */
+
+int _printf(const char *format, ...)
+{
+    int x = 0;
+
+    while (format[x] != '\0')
+    {
+        //Main stuff happens here
+    }
+
+    return (0);
+}
